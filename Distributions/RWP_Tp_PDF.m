6ebe5ffd54eb80affff54167ee56e132c9c00033
tathagatpal <55681355+tathagatpal@users.noreply.github.com>
2021-06-29 14:40:41 +0530

    Update RWP_Tp_PDF.m

--- Distributions/RWP_Tp_PDF.m
+++ Distributions/RWP_Tp_PDF.m
@@ -1,15 +1,10 @@
-a = 1;
-x = 0:0.1:a;
-y = 0:0.1:a;
+a = 5;
+x = 0:0.5:a;
+y = 0:0.5:a;
 E_L = 0.521405*a;
 
-% dist = 0:0.1:a;
-% f_init = poisspdf(dist, 10);
-
-ps = 0.1;
-ps2 = 0.3;
-v = 0.01;
-%v = 0.01*randi([5 15]);
+ps = [0.1 0.3];
+v = unifrnd(1,2);
 
 init_x = makedist('Uniform','lower',0,'upper',a);
 f_init_x = pdf(init_x, x);
@@ -19,22 +14,11 @@ f_init_y = pdf(init_y, y);
 
 f_init = f_init_x.*f_init_y;
 
-%f_m = 9/(16*a^6)*(x.^2-a.^2).*(y.^2-a.^2);
-%f_m = (-6/a^3*x.^2 + 6/a^2*x).*(-6/a^3*y.^2 + 6/a^2*y);
+f_tp = unifrnd(0,100);
+E_ftp = mean(f_tp);
 
-% tp = makedist('Uniform','lower',0,'upper',3);
-% f_tp = pdf(tp,x);
-% E_ftp = mean(f_tp);
-% 
-% tp2 = makedist('Uniform','lower',0,'upper',5);
-% f_tp2 = pdf(tp2, x);
-% E_ftp2 = mean(f_tp2);
-
-tp = 100;
-E_ftp = 100;
-
-tp2 = 300;
-E_ftp2 = 300;
+f_tp2 = unifrnd(100,300);
+E_ftp2 = mean(f_tp2);
 
 pause_prob = 1/(1 + (E_L/(v*E_ftp)));
 pause_prob2 = 1/(1 + (E_L/(v*E_ftp2)));
@@ -43,17 +27,30 @@ f_t = zeros(length(x),length(y));
 f_t2 = zeros(length(x),length(y));
 f_ntp = zeros(length(x),length(y));
 
+f_m_2D = (-6/a^3*x.^2 + 6/a^2.*x).*(-6/a^3*y.^2 + 6/a^2.*y);
+f_t_2D = ps(1).*f_init + (1-ps(1)).*pause_prob + (1-ps(1)).*(1-pause_prob).*f_m_2D;
+f_t_2D2 = ps(1).*f_init + (1-ps(1)).*pause_prob2 + (1-ps(1)).*(1-pause_prob2).*f_m_2D;
+
 for i=1:length(x)
     for j=1:length(y)
         f_m = (-6/a^3*x(i)^2 + 6/a^2*x(i))*(-6/a^3*y(j)^2 + 6/a^2*y(j));
         %f_m_ntp = 9/(16*a^6)*(x(i)^2-a^2)*(y(i)^2-a^2);
-        f_x_temp = ps*f_init(i) + (1-ps)*pause_prob + (1-ps)*(1-pause_prob)*f_m;
-        f_x_temp2 = ps2*f_init(i) + (1-ps2)*pause_prob2 + (1-ps2)*(1-pause_prob2)*f_m;
+        f_x_temp = ps(1)*f_init(i) + (1-ps(1))*pause_prob + (1-ps(1))*(1-pause_prob)*f_m;
+        %f_x_temp = 0.692 + 0.308*f_m;
+        f_x_temp2 = ps(1)*f_init(i) + (1-ps(1))*pause_prob2 + (1-ps(1))*(1-pause_prob2)*f_m;
+        %f_x_temp2 = 0.896 + 0.104*f_m;
         f_t(i,j) = f_x_temp;
         f_t2(i,j) = f_x_temp2;
         f_ntp(i,j) = f_m;
     end
 end
+
+
+% A1_inti= integral2(f_init, 0,5, 0,5);
+%A0 = trapz(x, f_init);
+A0 = trapz(x, f_init_x);
+A1 = trapz(x, f_t);
+A2 = trapz(x, f_t2);
 
 figure(1);
 [X,Y] = meshgrid(x,y);
@@ -66,16 +63,25 @@ zlabel('PDF')
 figure(2);
 [X,Y] = meshgrid(x,y);
 surf(X,Y,f_t);
-title('p_s = 0.1, t_p = 100s, v = 0.01');
+title('p_s = 0.1, t_p = unifrnd(0,100), v = 0.01');
 xlabel('x')
 ylabel('y')
 zlabel('PDF')
 
 figure(3);
 surf(X,Y,f_t2);
-title('p_s= 0.3, t_p = 300 s, v = 0.01');
+title('p_s= 0.1, t_p = unifrnd(100,300), v = 0.01');
 xlabel('x')
 ylabel('y')
 zlabel('PDF')
 
+figure(4);
+plot(x, f_t_2D, x, f_t_2D2, 'linewidth', 1.5);
+xlabel('position');
+ylabel('PDF');
+
+text_legend_1 = sprintf('t_p %.1f',f_tp);
+text_legend_2 = sprintf('t_p %.1f', f_tp2);
+legend(text_legend_1, text_legend_2);
+title('p_s= 0.1, t_p = 300 s, v = 0.01');
 
